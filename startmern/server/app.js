8ebd46001159f4749a26862b12ec3dd18c6d0562
kundan kumar sahu <kundanlal96580@gmail.com>
2023-07-18 23:38:14 +0530

    print hello world on the web

--- startmern/server/app.js
+++ startmern/server/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app  = express();
 
+
+const middleware = (req, res , next) => {
+    console.log('this is middleware...');
+    next();
+}
+
+
 app.get('/' , (req , res) =>{
     res.send('heloo from another stack');
 });
-app.get('/about' , (req , res) =>{
+app.get('/about',middleware , (req , res) =>{
     res.send('hey this is about..');
 });
 app.get('/contact' , (req , res) =>{
