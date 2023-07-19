f52ec613a910fa60a9003b232a9db1da7fbb0f96
kundan kumar sahu <kundanlal96580@gmail.com>
2023-07-19 23:33:32 +0530

    monogo connection

--- startmern/server/app.js
+++ startmern/server/app.js
@@ -1,6 +1,16 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app  = express();
+const PORT =3000;
+const DB = 'mongodb+srv://kundanlal96580:Wer6SjeCyBT1U415@cluster0.7orc3b1.mongodb.net/myfirstdb?retryWrites=true&w=majority';
 
+mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{
+    console.log("mongo connect sucessfully");
+})
+.catch((err)=>{
+    console.log(`mongoDB is not connected${err}`)
+})
 
 const middleware = (req, res , next) => {
     console.log('this is middleware...');
@@ -24,8 +34,7 @@ app.get('/signup' , (req , res) =>{
     res.send('hey this is signup..');
 });
 
-app.listen(3000 , () =>{
+app.listen(PORT , () =>{
     console.log('server is running on 3000.');
 });
-
  
