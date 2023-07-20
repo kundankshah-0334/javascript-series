bb47f673e44260f0080093520f56db1710177f97
kundan kumar sahu <kundanlal96580@gmail.com>
2023-07-21 00:12:25 +0530

    NPM started

---
+++ startmern/nodejs/npmMod/index.js
@@ -0,0 +1,12 @@
+const chalk = require("chalk");
+
+const validator = require("validator");
+
+console.log(("Hello chalk"));
+
+const valid = validator.isEmail('foo@bar.com');
+
+console.log(valid ? chalk.green.inverse(valid) : chalk.red.inverse(valid));
+
+
+ 
