bbe6c3a7bf6484fce69cee6d058340a07337b555
kundan kumar sahu <kundanlal96580@gmail.com>
2023-07-23 02:19:45 +0530

    Express started

---
+++ startmern/expressjs/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get("/" , (req , res) => {
+    res.send("Express js Started");
+});
+app.get("/about" , (req , res) => {
+    res.send("This is about page started");
+});
+
+app.listen(8000 , "127.0.0.1" , () => {
+    console.log('server is running on 8000')
+});
