4f0e6703736d12f06dafa7fdbb650c544bd92de4
kundan kumar sahu <kundanlal96580@gmail.com>
2023-07-24 01:05:54 +0530

    particials read

--- startmern/expressjs/src/index.js
+++ startmern/expressjs/src/index.js
@@ -1,15 +1,34 @@
 var path = require('path');
 var express = require('express');
+var hbs = require('hbs');
 var app = express();
 
 const staticPath = path.join(__dirname , "../public");
+const tmpPath = path.join(__dirname , "../template/views");
+const regiPar = path.join(__dirname , "../template/particials");
 
-app.use(express.static(staticPath));
+
+app.set('view engine' , "hbs");
+app.set('views' , tmpPath);
+hbs.registerPartials(regiPar);
+
+// app.use(express.static(staticPath));
+
 app.get("/" , (req , res) => {
-    res.send("Express js Started");
-});
+    res.render("index" , {
+        user_name : "kundan",
+    });
+}); 
 app.get("/about" , (req , res) => {
-    res.send("This is about page started");
+    res.render("about");
+}); 
+ 
+ 
+app.get("*" , (req , res) => {
+    res.render("404" , {
+        error : "This page does not exists.",
+    });
+
 });
 
 app.listen(8000 , "127.0.0.1" , () => {
