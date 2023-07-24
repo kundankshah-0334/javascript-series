3549d5e3504fa5abef058ce44c4a232f7d97dd30
kundan kumar sahu <kundanlal96580@gmail.com>
2023-07-25 01:18:55 +0530

    schema created

--- startmern/mongodb/mongoose/src/app.js
+++ startmern/mongodb/mongoose/src/app.js
@@ -1,9 +1,27 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/mymongodb', {useNewUrlParser: true , useUnifiedTopology : true})
+mongoose.connect('mongodb://localhost:27017/mywrmongodb', {useNewUrlParser: true , useUnifiedTopology : true})
 .then(()=>{
-    console.log("Connected to datbase");
+    console.log("Connected to database");
 })
 .catch((err) => {
     console.log(err);
 });
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Name is Required"]
+    } ,
+    age: Number,
+    course: String,
+    emailId : String,
+    city : String,
+    status:Boolean,
+    date:{
+        type:Date,
+        default: Date.now
+    }
+});
+
+const UserModel = new mongoose.model("User" , userSchema);
