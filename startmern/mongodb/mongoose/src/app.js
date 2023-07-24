88167749ae716cf4b69dec5c60598c74d37ee9e8
kundan kumar sahu <kundanlal96580@gmail.com>
2023-07-25 01:42:02 +0530

    data retrieved

--- startmern/mongodb/mongoose/src/app.js
+++ startmern/mongodb/mongoose/src/app.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/mywrmongodb', {useNewUrlParser: true , useUnifiedTopology : true})
+mongoose.connect('mongodb://localhost:27017/amongodb', {useNewUrlParser: true , useUnifiedTopology : true})
 .then(()=>{
     console.log("Connected to database");
 })
@@ -28,16 +28,56 @@ const UserModel = new mongoose.model("User" , userSchema);
 
 const userDocument = async () =>{
     try{
-        const user1 = new UserModel({
+        const user11 = new UserModel({
             name:"Raja",
             age: 78,
             course: "python",
             emailId : "Raja123@gmail.com",
             city : "Patna",
             status:true,
-        });
+        })
+        const user12 = new UserModel({
+            name:"Mohan",
+            age: 78,
+            course: "python",
+            emailId : "Raja123@gmail.com",
+            city : "Patna",
+            status:true,
+        })
+        const user13 = new UserModel({
+            name:"Prem",
+            age: 78,
+            course: "python",
+            emailId : "Raja123@gmail.com",
+            city : "Patna",
+            status:true,
+        })
+        const user14 = new UserModel({
+            name:"Janak",
+            age: 78,
+            course: "python",
+            emailId : "Raja123@gmail.com",
+            city : "Patna",
+            status:true,
+        })
+        const user15 = new UserModel({
+            name:"Karn",
+            age: 78,
+            course: "python",
+            emailId : "Raja123@gmail.com",
+            city : "Patna",
+            status:true,
+        })
+        const user16 = new UserModel({
+            name:"Raju",
+            age: 78,
+            course: "python",
+            emailId : "Raja123@gmail.com",
+            city : "Patna",
+            status:true,
+        })
         
-        const result = await user1.save();
+        const result = await UserModel.insertMany([user11,user12, user13, user14,user15, user16]);
         console.log(result);
 
     }catch(err){
@@ -45,5 +85,16 @@ const userDocument = async () =>{
     }
 };
 
-userDocument();
+// userDocument();
 
+const getDocument = async () =>{
+    try{
+        const result = await UserModel.find({course: "python"})
+        .select({name:1 ,_id:0})
+        .limit(1);
+        console.log(result);
+    }catch(err){
+        console.log(err);
+    }
+}
+getDocument();
