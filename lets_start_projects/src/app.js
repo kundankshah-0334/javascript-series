dbab122d728b6a60c5e7a9d6d4d74c6a83351296
kundan kumar sahu <kundanlal96580@gmail.com>
2023-07-29 04:30:44 +0530

    Registration complted

--- lets_start_projects/src/app.js
+++ lets_start_projects/src/app.js
@@ -5,12 +5,15 @@ const path =  require("path");
 const hbs =  require("hbs");
 const port = process.env.PORT || 8000;
 
+const Register = require("./models/registration")
+
 const static_path = path.join(__dirname , "../public");
 const views_path = path.join(__dirname , "../template/views");
 const partials_path = path.join(__dirname , "../template/partials");
  console.log(static_path);
  app.use(express.static(static_path));
 
+ app.use(express.urlencoded({extended : false}));
  app.set("view engine" , "hbs"); 
  app.set("views" , views_path); 
  hbs.registerPartials(partials_path);
@@ -26,6 +29,31 @@ app.get("/login" , (req , res) => {
 app.get("/register" , (req , res) => {
     // console.log( "Hello World!" );
     res.render("register");
+});
+
+app.post("/register" , async (req , res) => {
+     try{
+        const password = req.body.password ;
+        const cpassword = req.body.confirm_password ;
+
+        if(password == cpassword){
+            const userRegister = new Register ({
+                name: req.body.name,
+                email: req.body.email,
+                phone: req.body.phone,
+                 password : password,
+                 confirmpassword : cpassword
+            });
+
+            const registred = await userRegister.save();
+            res.status(201).render("index");
+        }else{
+            res.send("Password doed not match.")
+        }
+        
+     }catch(e) {
+        res.status(400).send(e);
+     }
 });
 
 app.listen(port  , () => {
