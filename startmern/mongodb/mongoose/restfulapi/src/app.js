0d7cb80265cf030daddd83ae59e3338249cd366f
kundan kumar sahu <kundanlal96580@gmail.com>
2023-07-28 22:23:20 +0530

    add routing

--- startmern/mongodb/mongoose/restfulapi/src/app.js
+++ startmern/mongodb/mongoose/restfulapi/src/app.js
@@ -12,6 +12,9 @@ const port = process.env.PORT || 8000 ;
 const validator = require("validator");
 const mongoose = require("mongoose");
 
+
+const studentRouter = require("./router/students.js")
+app.use(studentRouter);
 // app.post("/students" , (req , res) => {
 
 //     console.log(req.body);
@@ -23,66 +26,14 @@ const mongoose = require("mongoose");
 //     })
 // });
 
-app.post("/students" , async (req,res) => {
-    try{
-        const user =  new StudentModal(req.body);
-        const result = await user.save();
-        res.status(201).send(result);
-  }catch(e){
-    return res.status(500).send({error: e });
-  }
-});
- 
-app.get("/students" ,async(req , res) => {
-    try{
-        const studentData = await StudentModal.find();
-        res.status(201).send(studentData);
-    }catch(e){
-        res.status(400).send(e);
-    }
-});
+// const router = new express.Router();
 
-app.get("/students/:id", async (req , res) => {
-   try{
-        const _id = req.params.id;
-        const studentData = await StudentModal.findById(_id);
-        res.send(studentData);
+// router.get("/home" , (req ,res)=> {
+//     res.send("hello vhdf")
+// });
 
-   }catch(err){
-    res.status(500).send(err);
-   }
-});
- 
 
-app.patch("/students/:id" , async(req,res) => {
-    try{
-        const _id = req.params.id;
-        const result = await StudentModal.findByIdAndUpdate( _id , req.body , {new : true});
-        
-        return res.send(result);
-         
-         
-    }catch(e){
-        res.send.status(400).send(e);
-    }
-});
- 
 
-app.delete("/students/:id" , async(req,res) => {
-    try{
-        // const id = req.params.id;
-        const result = await StudentModal.findByIdAndDelete(req.params.id);
-        if(!req.params.id){
-            return res.send()
-            .status(400);
-        }
-        res.send(result);
-            // console.log(result);
-        
-    }catch(e){
-        res.send.status(500).send(e);
-    }
-});
  
 app.listen(port ,  () =>{
     console.log(`Connection succesful`);
