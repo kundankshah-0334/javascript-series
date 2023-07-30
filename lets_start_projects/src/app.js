0c4f00f9129425186e3cc9461f40e767bcce0b77
kundan kumar sahu <kundanlal96580@gmail.com>
2023-07-31 01:08:43 +0530

    cookies added

--- lets_start_projects/src/app.js
+++ lets_start_projects/src/app.js
@@ -88,10 +88,10 @@ app.post("/login" , async (req , res) => {
          const token = await userEmail.generateAuthToken();
          console.log(token);
 
-         res.cookie("jwt" , token , {
-            expires : new Date(Date.now() + 30000),
-            httpOnly : true
-           });
+        //  res.cookie("jwt" , token , {
+        //     expires : new Date(Date.now() + 30000),
+        //     httpOnly : true
+        //    });
 
 
 
