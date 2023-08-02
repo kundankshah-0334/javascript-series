8c09e109176fdb669de28c547d69506cdf1fd0e4
kundan kumar sahu <kundanlal96580@gmail.com>
2023-08-02 18:13:18 +0530

    project 2 started

---
+++ project_2/src/app.js
@@ -0,0 +1,31 @@
+const express =  require("express");
+const app = new express();
+const path = require("path");
+require("./db/conn");
+const PORT = process.env.PORT || 8000 ;
+const staticPath = path.join(__dirname ,"../public");
+
+app.use('/css' , express.static(path.join(__dirname , "../node_modules/bootstrap/dist/css")));
+app.use('/js' , express.static(path.join(__dirname , "../node_modules/bootstrap/dist/js")));
+app.use('/jq' , express.static(path.join(__dirname , "../node_modules/dist")));
+
+// app.use(express.static(staticPath));
+
+app.set('view engine' , "hbs");
+// console.log();
+
+
+app.get('/' , (req , res)=>{
+
+   res.send('from other side');
+
+});
+app.get('/about' , (req , res)=>{
+
+   res.send('hello from about  server :)')
+
+});
+
+app.listen(PORT , "127.0.0.1" , () =>{
+    console.log(`Server is running on port ${PORT}`)
+});
