54ff5d4cae954644638da1ae67381ab48550f2a3
kundan kumar sahu <kundanlal96580@gmail.com>
2023-08-06 22:12:51 +0530

    Third  method to print something on webpage

--- ReactJS/ReactTry/reactapp/src/index.js
+++ ReactJS/ReactTry/reactapp/src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React, { createElement } from 'react';
+// import ReactDOM from 'react-dom';
  
 
-ReactDOM.render(<h1>Hello  gooj fgrtg  !!</h1> 
-, document.getElementById("root"));
+// ReactDOM.render(<h1>Hello  gooj fgrtg  !!</h1> 
+// , document.getElementById("root"));
 
 // ReactDOM.render( /*#__PURE__*/React.createElement("h1", null ,  "Hello  dfg  gooj !!"
 //   ), document.getElementById("root"));
+
+var h1 = document.createElement("h1");
+h1.innerHTML = "Hello Maharaj";
+document.getElementById("root").appendChild(h1);
