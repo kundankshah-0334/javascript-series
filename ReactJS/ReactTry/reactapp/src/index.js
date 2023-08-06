c57a31993d5fa8b859291c8a4cecd98b1474470b
kundan kumar sahu <kundanlal96580@gmail.com>
2023-08-06 22:19:26 +0530

    Multiple tag displaying

--- ReactJS/ReactTry/reactapp/src/index.js
+++ ReactJS/ReactTry/reactapp/src/index.js
@@ -1,13 +1,24 @@
-// import React, { createElement } from 'react';
-// import ReactDOM from 'react-dom';
+import React, { createElement } from 'react';
+import ReactDOM from 'react-dom';
  
 
-// ReactDOM.render(<h1>Hello  gooj fgrtg  !!</h1> 
-// , document.getElementById("root"));
+ReactDOM.render(<h1>Hello  World!!</h1> 
+, document.getElementById("root"));
 
-// ReactDOM.render( /*#__PURE__*/React.createElement("h1", null ,  "Hello  dfg  gooj !!"
-//   ), document.getElementById("root"));
+        // ReactDOM.render(React.createElement("h1", null ,  "Hello  dfg  gooj !!"
+        //   ), document.getElementById("root"));
 
-var h1 = document.createElement("h1");
-h1.innerHTML = "Hello Maharaj";
-document.getElementById("root").appendChild(h1);
+        // var h1 = document.createElement("h1");
+        // h1.innerHTML = "Hello Maharaj";
+        // document.getElementById("root").appendChild(h1);
+
+
+ReactDOM.render(
+<>
+<h1>Hello  World!!</h1> 
+<h2>Hello  World!!</h2> 
+<p>Hello  World!!</p> 
+
+</>
+
+, document.getElementById("root"));
