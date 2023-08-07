57504f1cd448a394fcb7d1fec1810f2965983c14
kundan kumar sahu <kundanlal96580@gmail.com>
2023-08-07 23:51:31 +0530

    old code restored

--- ReactJS/ReactTry/reactapp/src/index.js
+++ ReactJS/ReactTry/reactapp/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
  
 
-ReactDOM.render(<h1>Hello  World!!</h1> 
-, document.getElementById("root"));
+// ReactDOM.render(<h1>Hello  World!!</h1> 
+// , document.getElementById("root"));
 
         // ReactDOM.render(React.createElement("h1", null ,  "Hello  dfg  gooj !!"
         //   ), document.getElementById("root"));
