2bd7f47f962cc6034663dded099059fa6279e884
kundan kumar sahu <kundanlal96580@gmail.com>
2023-08-09 23:23:08 +0530

    index.js over

--- ReactJS/good_morning/src/index.js
+++ ReactJS/good_morning/src/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import  ReactDOM from 'react-dom';
 
+let currDate = new Date();
+// let currHour = currDate.getHours();
+let currHour = 16;
+let message = "";
 
+const messageCss = {
+
+}
+const style = {
+    color : "",
+    textAlign : "center",
+    padding :"100px"
+  }
+
+if(currHour >= 6  && currHour < 12){
+    message = "Good Morining";
+    messageCss.color = "Yellow";
+} else if(currHour >= 12 && currHour < 16){
+    message = "Good Afternoon";
+    messageCss.color = "pink";
+} else if( currHour >= 16 && currHour < 20) {
+    message = "Good Evening";
+    messageCss.color = "Blue";
+} else{
+    message = "Good Night";
+    messageCss.color = "Black";
+}
 
 ReactDOM.render(
   <>
