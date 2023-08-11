4e3d4b24ded0b457bf07458c41f83cf9cf56ab84
kundan kumar sahu <kundanlal96580@gmail.com>
2023-08-11 23:40:13 +0530

    props started

---
+++ ReactJS/netflix/src/index.js
@@ -0,0 +1,23 @@
+ import React from 'react'
+ import ReactDOM from 'react-dom'
+ import "./index.css"
+
+ const a = '';
+
+ ReactDOM.render(
+  <>
+  <div class='Divv'>
+  <div class="card">
+  <img src="https://picsum.photos/200/300" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+
+
+ 
+  </>,document.getElementById("root")
+ )
