053ab6e7b79e3252af577a0b8cf5aef633e6ab3d
kundan kumar sahu <kundanlal96580@gmail.com>
2023-08-15 21:24:53 +0530

    getting time on every click

--- ReactJS/hooks/src/App.jsx
+++ ReactJS/hooks/src/App.jsx
@@ -1,28 +1,51 @@
-import React from "react";
+// import React, { useState } from "react";
+
+// const App = () => {
+  
+//       const state = useState();
+//       const [count , setCount] = useState(0);
+
+//       const IncreamentNumber = () => {
+//       setCount(count+1);
+//       // console.log("clicked" + count );
+// };
+
+//   return (
+//     <>
+//       <div class="container">
+//         <h1>{count}</h1>
+//         <button onClick={IncreamentNumber}>Click Me</button>
+//        </div>
+//     </>
+//   );
+// }
+// export default App;
 
 
-let count = 0;
 
-const IncreamentNumber = () => {
-  count++;
- console.log("clicked" + count );
- 
-
-}
+import React, { useState } from "react";
 
 const App = () => {
+  
+      let newtime  = new Date().toLocaleTimeString();
+
+      const [ctime , setTime] = useState(newtime);
+
+      const updateTime = () => {
+         newtime  = new Date().toLocaleTimeString();
+       setTime(newtime);
+};
+
   return (
     <>
-      {/* <h1>0</h1>
-
-      <button className="" >Click Me</button> */}
-
       <div class="container">
-    <h1>{count}</h1>
-    <button onClick={IncreamentNumber}>Click Me</button>
-  </div>
+        <h1>{ctime}</h1>
+        <button onClick={updateTime}>Get Time</button>
+       </div>
     </>
   );
 }
-
 export default App;
+
+
+
