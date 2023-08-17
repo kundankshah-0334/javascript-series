80b447a9c702b4aa581335cdcacd56e3305edb69
kundan kumar sahu <kundanlal96580@gmail.com>
2023-08-17 15:09:21 +0530

    Learned spread Operator

--- ReactJS/form/src/App.jsx
+++ ReactJS/form/src/App.jsx
@@ -8,43 +8,49 @@ const App = () => {
     Lname : "",
     email : "",
     phone : "",
+    hbs : "",
   });
 
     const valueEvent = (event) => {
 
     const {value , name } = event.target;
 
-    setName ( (preVal) => {
-      if(name==="Fname"){
-        return {
-              Fname : value,
-              Lname : preVal.Lname,
-              phone : preVal.phone,
-              email : preVal.email
-        }
+    setName ((preVal) => {
 
-      } else if(name==="Lname") {
-        return {
-              Fname : preVal.Fname,
-              Lname : value,
-              phone : preVal.phone,
-              email : preVal.email
-        }
-      } else if(name==="email") {
-        return {
-              Fname : preVal.Fname,
-              Lname : preVal.Lname,
-              phone : preVal.phone,
-              email : value
-        }
-      } else if(name==="phone") {
-        return {
-              Fname : preVal.Fname,
-              Lname : preVal.Lname,
-              phone : value,
-              email : preVal.email
-        }
-      }
+      return {
+        ...preVal,
+        [name] : value,
+      };
+      // if(name==="Fname"){
+      //   return {
+      //         Fname : value,
+      //         Lname : preVal.Lname,
+      //         phone : preVal.phone,
+      //         email : preVal.email
+      //   }
+
+      // } else if(name==="Lname") {
+      //   return {
+      //         Fname : preVal.Fname,
+      //         Lname : value,
+      //         phone : preVal.phone,
+      //         email : preVal.email
+      //   }
+      // } else if(name==="email") {
+      //   return {
+      //         Fname : preVal.Fname,
+      //         Lname : preVal.Lname,
+      //         phone : preVal.phone,
+      //         email : value
+      //   }
+      // } else if(name==="phone") {
+      //   return {
+      //         Fname : preVal.Fname,
+      //         Lname : preVal.Lname,
+      //         phone : value,
+      //         email : preVal.email
+      //   }
+      // }
     });
 
   };
@@ -62,29 +68,41 @@ const App = () => {
           <h1>Hello {fullName.Fname} {fullName.Lname}</h1>
           <p>{fullName.email}</p>
           <p>{fullName.phone}</p>
+          <p>{fullName.hbs}</p>
           <input
             type="text"
             placeholder="Enter First Name" 
             name="Fname"
             onChange={valueEvent}
+            value={fullName.Fname}
           /> 
           <input
             type="text"
             placeholder="Enter Second Name" 
             name="Lname"
             onChange={valueEvent}
+            value={fullName.Lname}
           /> 
           <input
             type="email"
             placeholder="Enter Email" 
             name="email"
             onChange={valueEvent}
+            value={fullName.email}
           /> 
           <input
             type="number"
             placeholder="Enter Mobile" 
             name="phone"
             onChange={valueEvent}
+            value={fullName.phone}
+          /> 
+          <input
+            type="text"
+            placeholder="Enter Your hobbies" 
+            name="hbs"
+            onChange={valueEvent}
+            value={fullName.hbs}
           /> 
           <button type="submit">Submit</button>
         </div>
