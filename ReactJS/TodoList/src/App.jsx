0387a3868699d9e0c31b3b902fba9f8a374ea811
kundan kumar sahu <kundanlal96580@gmail.com>
2023-08-18 21:01:53 +0530

    todo list project in process

--- ReactJS/TodoList/src/App.jsx
+++ ReactJS/TodoList/src/App.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+
+  const [ inputList , setInputList] = useState();
+
+  const [ items , setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  }
+  const listOfItems = () => {
+   setItems((oldItems) => {
+    return [ ...oldItems , inputList]
+   });
+   setInputList("");
+  }
+
  return  <>
     <div className="main_div">
       <div className="center_div">
         <br />
         <h1>ToDo List</h1>
         <br />
-        <input type="text" placeholder="Add a task" />
-        <button>+</button>
+        <input type="text" placeholder="Add a task" onChange={itemEvent} />
+        <button onClick={listOfItems}>+</button>
         <ol>
-          <li>Click Snap</li>
-          <li>Hit the Gym</li>
+ 
+
+          {
+            items.map((itemVal) => {
+              return <li>{itemVal}</li>
+            })
+          }
         </ol>
       </div>
     </div>
