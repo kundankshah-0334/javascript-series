b2ae9b6335bfd1f3ecc3c3072033fa887a53c1ce
kundan kumar sahu <kundanlal96580@gmail.com>
2023-08-19 15:18:09 +0530

    Learned mui icons and mui

--- ReactJS/incdec1/src/App.jsx
+++ ReactJS/incdec1/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
+import Button from '@material-ui/core/Button';
+// import Button from '@mui/material/Button';
 const App = () => {
 
   let [number , setNumber] = useState(0);
@@ -28,6 +30,7 @@ const App = () => {
               <button onClick={IncNum} className="btn1"><AddIcon /></button>
               <button onClick={DecNum} className="btn2"><RemoveIcon /></button>
               </div>
+         
           </div>
         </> 
       );
