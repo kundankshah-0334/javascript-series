3f5c88d452d012278178213a720154ea4f9dc58f
kundan kumar sahu <kundanlal96580@gmail.com>
2023-08-21 01:54:59 +0530

    google keep clone in process

--- ReactJS/googlekeep/src/Createnote.jsx
+++ ReactJS/googlekeep/src/Createnote.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 
 const Createnote = () => {
+ 
+    const [note , setNote] = useState({
+        title: "",
+        content : ""
+    });
+
+    const setData = (event) => {
+        const {name , value} = event.target;
+
+        setNote((preVal) => {
+            return {
+                ...preVal ,
+                [name] : value,
+            }
+        });
+
+        console.log(name)
+        }
+ 
+
   return (
     <>
     <div className="main_div">
        <form>
-        <input type="text" placeholder="Title" autoComplete="off" />
-        <textarea rows="" column="" placeholder="Type note" ></textarea>
+        <input type="text" name="title" value={note.title} onChange={setData} placeholder="Title" autoComplete="off" />
+        <textarea rows="" name="content" value={note.content} onChange={setData} column="" placeholder="Type note" ></textarea>
         <Button className="btn_add">
             <AddIcon className="add_btn" />
         </Button>
@@ -16,6 +36,6 @@ const Createnote = () => {
     </div>
     </>
   );
-}
+};
 
 export default Createnote;
